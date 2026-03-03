b495c4d5e6b8b6b8688f359f5ea2011c37e6ba58
Puskar Adhikari <puskar.ad@gmail.com>
2026-03-03 13:42:52 +0545

    fix: reorder imports for consistency in counters and _app components

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from "next/app";
 import "../styles/index.css";
 import "nextra-theme-docs/style.css";
+
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
