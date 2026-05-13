19e21700bacca471987affe265a072803bb4afd4
Puskar Adhikari <puskar.ad@gmail.com>
2026-05-14 01:31:13 +0545

    feat: add media query hooks

---
+++ src/hooks/useMediaQuery/index.ts
@@ -0,0 +1,5 @@
+export { breakpoints } from "./constants";
+export * from "./types";
+export { createBreakpointQueries, useBreakpoint } from "./useBreakpoint";
+export { useMediaQueries } from "./useMediaQueries";
+export { useMediaQuery } from "./useMediaQuery";
